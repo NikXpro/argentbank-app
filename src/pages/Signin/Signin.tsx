27a1ad41c9a98bdbc6525623da9add528635f108
NikX <41104633+NikXpro@users.noreply.github.com>
2025-01-27 18:45:19 +0100

    feat(signin): add navigation to user page on sign in - implement useNavigate hook for page redirection - update Button component import path for consistency - enhance Button to trigger navigation when clicked

--- src/pages/Signin/Signin.tsx
+++ src/pages/Signin/Signin.tsx
@@ -1,8 +1,11 @@
 import "@pages/Signin/Signin.scss";
-import { Button } from "../../components/Pages/Signin/Button";
+import { useNavigate } from "react-router";
+import { Button } from "../../components/Button";
 import { Input } from "../../components/Pages/Signin/Input";
 
 export function Signin() {
+  const navigate = useNavigate();
+
   return (
     <div className="main bg-dark">
       <section className="sign-in-content">
@@ -17,7 +20,13 @@ export function Signin() {
             type="checkbox"
             isRemember
           />
-          <Button />
+          <Button
+            onClick={() => {
+              navigate("/user");
+            }}
+          >
+            Sign In
+          </Button>
         </form>
       </section>
     </div>
