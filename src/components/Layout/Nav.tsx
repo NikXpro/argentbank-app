820d719681dafb4f562c29c4c72cc198a692b8e0
NikX <41104633+NikXpro@users.noreply.github.com>
2025-01-27 18:45:05 +0100

    feat(components): replace Link with NavLink for navigation - switch from Link to NavLink for better active link styling - update imports and component usage in the Nav component

--- src/components/Layout/Nav.tsx
+++ src/components/Layout/Nav.tsx
@@ -1,21 +1,21 @@
-import { Link } from "react-router";
+import { NavLink } from "react-router";
 import "./Nav.scss";
 export function Nav() {
   return (
     <nav className="main-nav">
-      <Link to="/" className="main-nav-logo">
+      <NavLink to="/" className="main-nav-logo">
         <img
           className="main-nav-logo-image"
           src="./img/argentBankLogo.png"
           alt="Argent Bank Logo"
         />
         <h1 className="sr-only">Argent Bank</h1>
-      </Link>
+      </NavLink>
       <div>
-        <Link to="/signin" className="main-nav-item">
+        <NavLink to="/signin" className="main-nav-item">
           <i className="fa fa-user-circle"></i>
           <span> Sign In</span>
-        </Link>
+        </NavLink>
       </div>
     </nav>
   );
