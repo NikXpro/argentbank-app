562725af759c2d3223bca42dcde32f089527dcbc
NikX <41104633+NikXpro@users.noreply.github.com>
2025-01-27 14:35:09 +0100

    feat(nav): add navigation component and styles - create Nav component with links to home and user pages - add styling for navigation layout and elements

---
+++ src/components/Layout/Nav.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router";
+import "./Nav.scss";
+export function Nav() {
+  return (
+    <nav className="main-nav">
+      <Link to="/" className="main-nav-logo">
+        <img
+          className="main-nav-logo-image"
+          src="./img/argentBankLogo.png"
+          alt="Argent Bank Logo"
+        />
+        {/* <h1 className="sr-only">Argent Bank</h1> */}
+      </Link>
+      <div>
+        <Link to="/user" className="main-nav-item">
+          <i className="fa fa-user-circle"></i>
+          <span>Sign In</span>
+        </Link>
+      </div>
+    </nav>
+  );
+}
