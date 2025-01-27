73f2f9719c70ec0adbf25ab5ac87bb8a88f9802c
NikX <41104633+NikXpro@users.noreply.github.com>
2025-01-27 21:44:50 +0100

    feat(button): improve button styling and props - rename .sign-in-button class to .button for better reusability - add padding adjustments for enhanced appearance - introduce optional type and className props for flexibility

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,11 +3,13 @@ import "@components/Button.scss";
 type ButtonProps = {
   children: React.ReactNode;
   onClick?: () => void;
+  type?: "button" | "submit" | "reset";
+  className?: string;
 };
 
-export function Button({ children, onClick }: ButtonProps) {
+export function Button({ children, onClick, type, className }: ButtonProps) {
   return (
-    <button className="sign-in-button" onClick={onClick}>
+    <button className={`button ${className}`} onClick={onClick} type={type}>
       {children}
     </button>
   );
