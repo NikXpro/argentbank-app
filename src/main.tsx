6cdcc735e40bfea9ae4d4576d084f63d5b1f0147
NikX <41104633+NikXpro@users.noreply.github.com>
2025-01-27 14:35:56 +0100

    feat(main): update routing structure and styles - add Layout component for consistent page structure - implement user route for future development - switch from CSS to SCSS for improved styling capabilities

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,18 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
-import "./index.css";
+import { Layout } from "./components";
+import "./index.scss";
 import { Home } from "./pages";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="/" element={<Home />} />
+          <Route path="/user" element={""} />
+        </Route>
       </Routes>
     </BrowserRouter>
   </StrictMode>
