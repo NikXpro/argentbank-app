7c4066d8140ea75b4e4f8a2e89a12b37fdca523e
NikX <41104633+NikXpro@users.noreply.github.com>
2025-01-27 17:47:44 +0100

    fix(main): update import path for Layout component - adjust import statement to include the correct subdirectory for Layout component - ensure consistency and clarity in component imports

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Routes } from "react-router";
-import { Layout } from "./components";
+import { Layout } from "./components/Layout";
 import "./index.scss";
 import { Home } from "./pages";
 
