4f7241f3a8a29974aa594e805c87f92fcbe4fcf8
NikX <41104633+NikXpro@users.noreply.github.com>
2025-02-27 13:13:51 +0100

    feat(main): integrate redux with provider and update routes - add Provider component to connect Redux store - change signin route path from "/signin" to "/login" for consistency

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,26 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { Layout } from "./components/Layout";
 import "./index.scss";
 import { Home } from "./pages";
 import { Signin } from "./pages/Signin/Signin";
 import { User } from "./pages/User/User";
+import { store } from "./store/store";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route path="/" element={<Home />} />
-          <Route path="/signin" element={<Signin />} />
-          <Route path="/user" element={<User />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/login" element={<Signin />} />
+            <Route path="/user" element={<User />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </StrictMode>
 );
