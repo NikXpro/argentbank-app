5fe3fc333bfc14d610df563edaebeca8a97ab121
NikX <41104633+NikXpro@users.noreply.github.com>
2025-06-02 19:13:42 +0200

    fix(main): update user import path to new directory structure

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { Layout } from "./components/Layout";
 import "./index.scss";
 import { Home } from "./pages";
 import { Signin } from "./pages/Signin/Signin";
-import { User } from "./pages/profile/profile";
+import { User } from "./pages/User/User";
 import { store } from "./store/store";
 
 createRoot(document.getElementById("root")!).render(
